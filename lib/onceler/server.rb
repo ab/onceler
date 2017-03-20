ab/onceler

commit 339efa11d8af62f46159591c8276861c11454a9a

Refactor lots of things, add multi clip mode.

lib/onceler/server.rb
@@ -5,7 +5,7 @@ module Onceler
   class Server < Sinatra::Base
     set :bind, '127.0.0.1'
     set :port, 6623
-    disable :show_exceptions
+
     enable :logging, :dump_errors
 
     set :root, Onceler.root_folder
@@ -23,18 +23,49 @@ module Onceler
       erb :not_found
     end
 
+    error do
+      erb :error
+    end
+
     get '/' do
       erb :root
     end
 
     get '/once/:key/' do |key|
       begin
-        @data = @@cache.get(key)
+        @entry = @@cache.get(key)
       rescue IndexError
         raise Sinatra::NotFound
       end
 
       erb :info
+    end
+
+    # you can view content of multi-use keys with GET request
+    get '/clip/:key/' do |key|
+      begin
+        entry = @@cache.get(key)
+      rescue IndexError
+        # TODO: rename :root to :create
+        @mode = :multi
+        @path = request.path
+        @key = key
+        halt erb(:root)
+      end
+
+      # Invalid to try to access a single-use key as multi-use.
+      if entry.once?
+        raise Sinatra::NotFound
+      end
+
+      if entry.attachment?
+        attacentry entry.filename
+        entry.content
+      else
+        @entry = entry
+        @delete_link = entry.url(request.url, delete_link: true)
+        erb :fetch
+      end
     end
 
     get '/once/' do
@@ -48,38 +79,41 @@ module Onceler
         filename = nil
       end
       content = params.fetch('content')
-      data = Entry.new(content, request.host, request.ip, filename)
 
-      key = @@cache.add(data)
-
-      # make an absolute URL relative to the user-provided URL
-      uri = URI.parse(request.url)
-      uri.path = "/once/#{key}/"
-      uri.query = nil
-      uri.fragment = nil
-      @secret_url = uri.to_s
-
-      unless ['http', 'https'].include?(uri.scheme)
-        halt 400, 'ERROR: Cowardly refusing to make URI of type ' + uri.scheme
+      case params.fetch('action')
+      when 'once'
+        once = true
+      when 'multi'
+        once = false
+      else
+        raise ArgumentError.new("Invalid action: " + params['action'].inspect)
       end
 
-      @@cache.add(data)
+      key = params.fetch('key', nil)
+
+      entry = Entry.new(content, request.host, request.ip, filename:filename,
+                       once: once, key: key)
+      @@cache.add_entry(entry)
+
+      # make an absolute URL relative to the user-provided URL
+      @secret_url = entry.url(request.url)
 
       erb :link
     end
 
     post '/once/:key/' do |key|
       begin
-        data = @@cache.delete(key)
+        entry = @@cache.delete(key)
       rescue IndexError
         raise Sinatra::NotFound
       end
 
-      if data.attachment?
-        attachment data.filename
-        data.content
+      if entry.attachment?
+        attacentry entry.filename
+        entry.content
       else
-        @data = data
+        @entry = entry
+        @deleted = true
         erb :fetch
       end
     end

lib/onceler.rb
@@ -9,6 +9,9 @@ module Onceler
   def self.root_folder
     File.join(File.dirname(__FILE__), '..')
   end
+
+  # Base Onceler exception class
+  class Error < StandardError; end
 end
 
 require_relative './onceler/cache'

lib/onceler/cache.rb
@@ -29,11 +29,15 @@ module Onceler
       @data.delete(key) or raise IndexError.new("key #{key.inspect} not found")
     end
 
+    def add_entry(entry)
+      @data[entry.key] = entry
+    end
+
     def add_by_key(key, value)
       @data[key] = value
     end
 
-    def add(value)
+    def add_random(value)
       key = self.class.random_key
       add_by_key(key, value)
       key

lib/onceler/entry.rb
@@ -1,17 +1,46 @@
 module Onceler
   class Entry
-    attr_reader :content, :remote_host, :remote_ip, :filename, :created
+    attr_reader :key, :content, :remote_host, :remote_ip, :filename, :created
 
-    def initialize(content, remote_host, remote_ip, filename=nil)
+    def initialize(content, remote_host, remote_ip, filename:nil, once: true, key: nil)
       @content = content
       @remote_host = remote_host
       @remote_ip = remote_ip
       @filename = filename
+      @once = once
       @created = Time.now.to_i
+
+      @key = key || Onceler::Cache.random_key
     end
 
     def attachment?
-      not @filename.nil?
+      !!filename
+    end
+
+    def once?
+      !!@once
+    end
+
+    # Make an absolute URL relative to the user-provided request_url.
+    #
+    # @param request_url [String]
+    # @return [String]
+    #
+    def url(request_url, delete_link: false)
+      uri = URI.parse(request_url)
+      if @once || delete_link
+        uri.path = "/once/#{key}/"
+      else
+        uri.path = "/clip/#{key}/"
+      end
+      uri.query = nil
+      uri.fragment = nil
+
+      unless ['http', 'https'].include?(uri.scheme)
+        raise ArgumentError.new("Bad URI scheme #{uri.inspect}")
+      end
+
+      return uri.to_s
     end
   end
 end

views/error.erb
@@ -1,0 +1,22 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Onceler - Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>
+      The onceler listens to your secrets and gives you a link to share them
+      exactly once.
+    </p>
+    <h3>
+      Something went wrong, sorry!
+    </h3>
+    <p><a href="https://github.com/ab/onceler/issues">File a bug?</a></p>
+    <p><a href="/">Back to home</a></p>
+    <img src="/img/lorax-ohno.jpg" alt="lorax" style="width: 100%" />
+    <p>
+    </p>
+  </body>
+</html>
+<% # vim: set ft=eruby sw=2 ts=2 tw=79: %>

views/fetch.erb
@@ -17,14 +17,23 @@
       for the secrets I tell you are for your ears alone.
     </h3>
 
-    <pre id="secret"><%= @data.content %></pre>
+    <pre id="secret"><%= @entry.content %></pre>
 
+<% if @entry.once? || @deleted %>
     <p>
       This secret will be gone forever when you refresh.
       It has already been deleted from the server.
     </p>
+<% else %>
+    <p>
+      This is a multi-use secret.
+      <form method="post" action="<%= @delete_link or raise 'delete_link' %>">
+        <button type="submit">Delete</button>
+      </form>
+    </p>
+<% end %>
 
-    <p>This secret was created from <%= @data.remote_host %> at <%= Time.at(@data.created) %></p>
+    <p>This secret was created from <%= @entry.remote_host %> at <%= Time.at(@entry.created) %></p>
 
     <p>
       <a href="../../">Share your own secret</a>

views/info.erb
@@ -17,7 +17,7 @@
       (Button will only work once.)
     </form>
 
-    <p>This secret was created from <%= @data.remote_host %> at <%= Time.at(@data.created) %></p>
+    <p>This secret was created from <%= @entry.remote_host %> at <%= Time.at(@entry.created) %></p>
 
     <p>
       <a href="../../">Share your own secret</a>

commit a06476e932ebdc9f281e95743c7729e8e6267aac

Fix "attachment" bug/typo.
Bug introduced by 339efa11d8af62f46159591c8276861c11454a9a

lib/onceler/server.rb
@@ -59,7 +59,7 @@ module Onceler
       end
 
       if entry.attachment?
-        attacentry entry.filename
+        attachment entry.filename
         entry.content
       else
         @entry = entry
@@ -109,7 +109,7 @@ module Onceler
       end
 
       if entry.attachment?
-        attacentry entry.filename
+        attachment entry.filename
         entry.content
       else
         @entry = entry